7cf13bd971d78cd9c1903a7e4e42c9714a968ee7
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-07-05 19:28:00 -0600

    merge conflict

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 var button = document.getElementById('guess')
-<<<<<<< HEAD
 var newInput = document.getElementById('input-value')
-var showInput = document.querySelector('.output')
-var deleteInput = document.querySelector('.clear')
-
-button.addEventListener('click', function() {
-  var showMe = newInput.value
-  showInput.innerText = showMe;
-})
-
-deleteInput.addEventListener('click', function(){
-  showInput.innerText = "";
-})
-=======
-var newInput = document.querySelector('#input-value')
 var showInput = document.querySelector('.output')
 var deleteInput = document.querySelector('.clear')
 
@@ -24,7 +10,4 @@ button.addEventListener('click', function () {
 
 deleteInput.addEventListener('click', function() {
     showInput.innerText = ""
-
-
 });
->>>>>>> master
